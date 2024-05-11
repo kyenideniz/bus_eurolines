e3fe3916028de12c298cdcd2ee1eab1f8a8dc587
kyenideniz <keremefeyenideniz@gmail.com>
2024-05-11 20:55:07 +0300

    modified:   components/pickRoute.tsx

--- components/pickRoute.tsx
+++ components/pickRoute.tsx
@@ -1,7 +1,16 @@
 "use client"
 
 import * as React from "react"
+import {
+  ArrowUpCircle,
+  CheckCircle2,
+  Circle,
+  HelpCircle,
+  LucideIcon,
+  XCircle,
+} from "lucide-react"
 
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
   Command,
@@ -11,7 +20,6 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command"
-
 import {
   Popover,
   PopoverContent,
@@ -21,28 +29,34 @@ import {
 type Status = {
   value: string
   label: string
+  icon: LucideIcon
 }
 
 const statuses: Status[] = [
   {
     value: "backlog",
     label: "Backlog",
+    icon: HelpCircle,
   },
   {
     value: "todo",
     label: "Todo",
+    icon: Circle,
   },
   {
     value: "in progress",
     label: "In Progress",
+    icon: ArrowUpCircle,
   },
   {
     value: "done",
     label: "Done",
+    icon: CheckCircle2,
   },
   {
     value: "canceled",
     label: "Canceled",
+    icon: XCircle,
   },
 ]
 
@@ -52,49 +66,60 @@ export function PickRoute() {
     null
   )
 
-    return (
+  return (
+    <div className="flex items-center space-x-4">
+      <p className="text-sm text-muted-foreground">Status</p>
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant="outline" className="m-4 w-full">
-            {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-[150px] justify-start"
+          >
+            {selectedStatus ? (
+              <>
+                <selectedStatus.icon className="mr-2 h-4 w-4 shrink-0" />
+                {selectedStatus.label}
+              </>
+            ) : (
+              <>+ Set status</>
+            )}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0" align="start">
-          <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+        <PopoverContent className="p-0" side="right" align="start">
+          <Command>
+            <CommandInput placeholder="Change status..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup>
+                {statuses.map((status) => (
+                  <CommandItem
+                    key={status.value}
+                    value={status.value}
+                    onSelect={(value) => {
+                      setSelectedStatus(
+                        statuses.find((priority) => priority.value === value) ||
+                          null
+                      )
+                      setOpen(false)
+                    }}
+                  >
+                    <status.icon
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        status.value === selectedStatus?.value
+                          ? "opacity-100"
+                          : "opacity-40"
+                      )}
+                    />
+                    <span>{status.label}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
         </PopoverContent>
       </Popover>
-    )
-}
-
-function StatusList({
-  setOpen,
-  setSelectedStatus,
-}: {
-  setOpen: (open: boolean) => void
-  setSelectedStatus: (status: Status | null) => void
-}) {
-  return (
-    <Command>
-      <CommandInput placeholder="Filter status..." />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup>
-          {statuses.map((status) => (
-            <CommandItem
-              key={status.value}
-              value={status.value}
-              onSelect={(value) => {
-                setSelectedStatus(
-                  statuses.find((priority) => priority.value === value) || null
-                )
-                setOpen(false)
-              }}
-            >
-              {status.label}
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      </CommandList>
-    </Command>
+    </div>
   )
 }
