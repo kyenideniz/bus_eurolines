a01acd9d3a2bc54b8f2ee54f3c8d292f3942dba5
kyenideniz <keremefeyenideniz@gmail.com>
2024-05-25 00:45:10 +0300

    main push

--- components/ticketPopover.tsx
+++ components/ticketPopover.tsx
@@ -91,6 +91,7 @@ const steps = [
                     index={0}
                 />
                 </div>
+                
             </div>
             <div className="w-full h-full text-black items-center justify-center flex">
                 <div className="w-full max-w-6xl h-full">
