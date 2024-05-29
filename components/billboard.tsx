c68347d62e8c811d8f3cb83058f507d594bd24da
kyenideniz <keremefeyenideniz@gmail.com>
2024-05-29 15:28:28 +0300

    main commit

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -78,7 +78,7 @@ export default function Billboard() {
     } else if (allFieldsFilled) {
       return (
         <Button onClick={() => {
-          console.log("toast")
+          console.log("toasta")
           toast({
             variant: "destructive",
             title: "Please pick different places!",
